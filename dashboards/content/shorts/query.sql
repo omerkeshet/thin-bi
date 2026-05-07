9743a39d73bbbabd2e83a9505734d0af37cbc767
omerkeshet <omer.yarchi@keshet-d.com>
2026-05-07 15:29:48 +0300

    Update query.sql

--- dashboards/content/shorts/query.sql
+++ dashboards/content/shorts/query.sql
@@ -1,3 +1,8 @@
-SELECT *
+SELECT
+    CAST("date" AS DATE) AS "date",
+    "site",
+    SUM("plays") AS "plays"
 FROM POC_DATABASE."domo"."shorts_all_sites_agg"
-WHERE CAST("date" AS DATE) >= DATEADD(DAY, -2, CURRENT_DATE());
+WHERE CAST("date" AS DATE) >= DATEADD(DAY, -7, CURRENT_DATE())
+GROUP BY 1, 2
+ORDER BY 1, 2;
