d7cce21f66bc2bb1ffe2eb7d888cc7bc661a172b
omerkeshet <omer.yarchi@keshet-d.com>
2026-05-07 17:20:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def _load_logo_svg() -> str | None:
 # Page config + global styling
 # ---------------------------------------------------------------------------
 
-_logo_svg = None
+_logo_svg = _load_logo_svg()
 
 st.set_page_config(
     page_title="OmerBI",
