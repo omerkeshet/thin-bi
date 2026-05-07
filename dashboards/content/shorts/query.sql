6f67532ac4c4091458a71b8f1694f2ad0228159b
omerkeshet <omer.yarchi@keshet-d.com>
2026-05-07 15:26:27 +0300

    Update query.sql

--- dashboards/content/shorts/query.sql
+++ dashboards/content/shorts/query.sql
@@ -1,3 +1,3 @@
 SELECT *
 FROM POC_DATABASE."domo"."shorts_all_sites_agg"
-WHERE CAST("date" AS DATE) >= DATEADD(DAY, -7, CURRENT_DATE());
+WHERE CAST("date" AS DATE) >= DATEADD(DAY, -2, CURRENT_DATE());
